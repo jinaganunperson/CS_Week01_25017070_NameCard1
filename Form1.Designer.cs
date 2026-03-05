6c4846313f3240739d58eba45546d6d58877c00c
06you <06you@>
2026-03-05 18:23:05 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.ForeColor = Color.Brown;
+            button1.Location = new Point(666, 162);
+            button1.Name = "button1";
+            button1.Size = new Size(260, 46);
+            button1.TabIndex = 0;
+            button1.Text = "배경색 바꾸기";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("휴먼둥근헤드라인", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(502, 162);
+            label1.Name = "label1";
+            label1.Size = new Size(152, 45);
+            label1.TabIndex = 1;
+            label1.Text = "장윤서\r\n";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(68, 97);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(378, 359);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 13.125F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label2.ForeColor = Color.FromArgb(128, 64, 0);
+            label2.Location = new Point(502, 231);
+            label2.Name = "label2";
+            label2.Size = new Size(484, 47);
+            label2.TabIndex = 3;
+            label2.Text = "수원대 컴퓨터SW학과 재학생";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(502, 292);
+            label3.Name = "label3";
+            label3.Size = new Size(359, 45);
+            label3.TabIndex = 4;
+            label3.Text = "06younseo@naver.com";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(502, 361);
+            label4.Name = "label4";
+            label4.Size = new Size(244, 45);
+            label4.TabIndex = 5;
+            label4.Text = "010-2314-2998";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(18F, 45F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1011, 583);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,23 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random(); 
+            this.BackColor = Color.FromArgb(rand.Next(200), rand.Next(200), rand.Next(256));
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
